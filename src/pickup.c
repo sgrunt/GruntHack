18fccda230bb995afd50276018480e858de4d2b1
Steve Melenchuk <smelenchuk@gmail.com>
2012-04-19 21:07:11 -0600

    Don't drop out of the item identification menu if scrolling past the end (but still allow it to be escaped from). Fixes C343-349.

--- src/pickup.c
+++ src/pickup.c
@@ -687,6 +687,8 @@ menu_item **pick_list;	/* list of objects and counts to pick up */
  *	USE_INVLET	  - Use object's invlet.
  *	INVORDER_SORT	  - Use hero's pack order.
  *	SIGNAL_NOMENU	  - Return -1 rather than 0 if nothing passes "allow".
+ *	FEEL_COCKATRICE   - Engage cockatrice checks and react appropriately.
+ *	SIGNAL_ESCAPE     - Return -2 if menu is escaped.
  */
 int
 query_objlist(qstr, olist, qflags, pick_list, how, allow)
@@ -824,7 +826,7 @@ boolean FDECL((*allow), (OBJ_P));/* allow function */
 		if (mi->count == -1L || mi->count > mi->item.a_obj->quan)
 		    mi->count = mi->item.a_obj->quan;
 	} else if (n < 0) {
-	    n = 0;	/* caller's don't expect -1 */
+	    n = (qflags & SIGNAL_ESCAPE) ? -2 : 0;
 	}
 	return n;
 }
